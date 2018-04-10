cf021157a7b920518280dc82d19eab9dd61d6568
Dan Kolbman <dankolbman@gmail.com>
2018-04-10 12:53:50 -0400

    :wrench: Set order everything by latest

--- coordinator/api/views.py
+++ coordinator/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.mixins import UpdateModelMixin
 from coordinator.api.models import Task, TaskService, Release
 from coordinator.api.serializers import (
     TaskSerializer,
@@ -12,7 +13,7 @@ class TaskViewSet(viewsets.ModelViewSet):
     Endpoint for tasks
     """
     lookup_field = 'kf_id'
-    queryset = Task.objects.all()
+    queryset = Task.objects.order_by('-created_at').all()
     serializer_class = TaskSerializer
 
 
@@ -21,14 +22,16 @@ class TaskServiceViewSet(viewsets.ModelViewSet):
     Endpoint for task services
     """
     lookup_field = 'kf_id'
-    queryset = TaskService.objects.all()
+    queryset = TaskService.objects.order_by('-created_at').all()
     serializer_class = TaskServiceSerializer
 
 
-class ReleaseViewSet(viewsets.ModelViewSet):
+class ReleaseViewSet(viewsets.ModelViewSet, UpdateModelMixin):
     """
     endpoint for releases
     """
     lookup_field = 'kf_id'
-    queryset = Release.objects.all()
+    queryset = Release.objects.order_by('-created_at').all()
     serializer_class = ReleaseSerializer
+
+
