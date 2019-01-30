1c4f178d8db7b688aedae3c8a065872ecd5b076f
Dan Kolbman <dankolbman@gmail.com>
2019-01-30 14:11:46 -0500

    :label: Release 1.5.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="kf-api-coordinator",
-    version="1.5.1",
+    version="1.5.2",
     description="Release Coordinator",
     license="Apache 2",
     packages=find_packages()
