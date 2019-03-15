f584d9700c78d7d1d20f8beef15a70dc402300dc
Dan Kolbman <dankolbman@gmail.com>
2019-03-15 13:24:36 -0400

    :sparkles: Add auth0 authentication

--- coordinator/authentication.py
+++ coordinator/authentication.py
@@ -1,5 +1,6 @@
 import datetime
 import jwt
+import json
 import re
 import textwrap
 import requests
@@ -95,6 +96,88 @@ class EgoAuthentication(authentication.BaseAuthentication):
         user = token['context']['user']
 
         return (user, None)
+
+
+class Auth0Authentication(authentication.BaseAuthentication):
+
+    @staticmethod
+    def _get_auth0_key():
+        """
+        Attempts to retrieve the ego public key from the cache. If it's not
+        there or is expired, fetch a new one from ego and store it back in the
+        cache.
+        """
+        key = cache.get(settings.CACHE_AUTH0_KEY, None)
+        # If key is not set in cache (or has timed out), get a new one
+        if key is None:
+            key = Auth0Authentication._get_key()
+            cache.set(settings.CACHE_AUTH0_KEY, key,
+                      settings.CACHE_AUTH0_TIMEOUT)
+        return key
+
+    @staticmethod
+    def _get_new_key():
+        """
+        Get a public key from Auth0's JWKS
+        Reformat the JWKS into a PEM format
+        """
+        resp = requests.get(settings.AUTH0_JWKS, timeout=10)
+        key = resp.json()['keys'][0]
+        public_key = jwt.algorithms.RSAAlgorithm.from_jwk(json.dumps(key))
+        return public_key
+
+    def authenticate(self, request):
+        """
+        Attempt to authenticate if there is an `Authorization` header
+        with a token is prefixed with `Bearer `.
+
+        :returns: The JWT context if a valid JWT was in the header,
+            None otherwise.
+        :raises: AuthenticationFailed if the token is not valid
+        """
+        if settings.DEBUG:
+            return ({'roles': 'ADMIN'}, None)
+
+        token = request.META.get('HTTP_AUTHORIZATION')
+        if token is None:
+            header = request.META.get('headers')
+            # No headers
+            if header is None:
+                return ({}, None)
+            token = header.get('Authorization', None)
+        # No Authorization header
+        if token is None:
+            return ({}, None)
+        # Unexpected token type
+        if 'Bearer ' not in token:
+            return ({}, None)
+
+        token = token.split('Bearer ')[-1]
+
+        try:
+            public_key = Auth0Authentication._get_new_key()
+            token = jwt.decode(token, public_key, algorithms='RS256',
+                               options={'verify_aud': False})
+        except (TypeError, KeyError):
+            # If we had trouble getting JWKS
+            return None
+        except jwt.exceptions.DecodeError as err:
+            raise exceptions.AuthenticationFailed(
+                f'Problem authenticating request: {err}'
+            )
+        except jwt.exceptions.InvalidTokenError as err:
+            raise exceptions.AuthenticationFailed(
+                f'Token provided is not valid: {err}'
+            )
+
+        token['permissions'] = token['https://kidsfirstdrc.org/permissions']
+        token['groups'] = token['https://kidsfirstdrc.org/groups']
+        token['roles'] = token['https://kidsfirstdrc.org/roles']
+        del token['https://kidsfirstdrc.org/permissions']
+        del token['https://kidsfirstdrc.org/groups']
+        del token['https://kidsfirstdrc.org/roles']
+
+        return (token, None)
 
 
 def get_service_token():
