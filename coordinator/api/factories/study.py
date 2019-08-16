d3522e5f9d932f034251bc6f176c246923d5330b
Dan Kolbman <dankolbman@gmail.com>
2019-08-16 13:46:52 -0400

    :factory: Add release note factory

--- coordinator/api/factories/study.py
+++ coordinator/api/factories/study.py
@@ -6,6 +6,7 @@ from coordinator.api.models.study import Study
 class StudyFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Study
+        django_get_or_create = ("kf_id",)
 
     kf_id = "SD_TESTTEST"
     name = factory.Faker("bs")
