7f4678675f234647b7666eb8781b25226d1e565c
Dan Kolbman <dankolbman@gmail.com>
2019-08-26 09:06:40 -0400

    :factory: Update task and release factories

--- coordinator/api/factories/task.py
+++ coordinator/api/factories/task.py
@@ -1,13 +1,15 @@
 import factory
 
-from coordinator.api.factories.release import ReleaseFactory
-from coordinator.api.factories.task_service import TaskServiceFactory
 from coordinator.api.models.task import Task
+
 
 class TaskFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Task
 
-
-    release = factory.SubFactory(ReleaseFactory)
-    task_service = factory.SubFactory(TaskServiceFactory)
+    release = factory.SubFactory(
+        "coordinator.api.factories.release.ReleaseFactory"
+    )
+    task_service = factory.SubFactory(
+        "coordinator.api.factories.task_service.TaskServiceFactory"
+    )
