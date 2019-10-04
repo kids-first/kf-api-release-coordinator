4f5d46e290f10510cf1ce503ccefe2ec991b768a
Dan Kolbman <dankolbman@gmail.com>
2019-10-04 13:40:53 -0400

    :sparkles: Add mutation to create task services

--- coordinator/graphql/task_services.py
+++ coordinator/graphql/task_services.py
@@ -1,22 +1,25 @@
+import graphene
+from graphql import GraphQLError
+from django.core.exceptions import ValidationError
 from django_filters import CharFilter, FilterSet, NumberFilter, OrderingFilter
-from graphene import relay, ObjectType, String
 from graphene_django.types import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
 from coordinator.api.models.taskservice import TaskService
+from coordinator.api.validators import validate_endpoint
 
 
 class TaskServiceNode(DjangoObjectType):
     """ A task service in the TaskService Coordinator """
 
-    health_status = String(
+    health_status = graphene.String(
         resolver=lambda obj, _: obj.health_status,
         description="Current status of the task service",
     )
 
     class Meta:
         model = TaskService
-        interfaces = (relay.Node,)
+        interfaces = (graphene.relay.Node,)
 
 
 class TaskServiceFilter(FilterSet):
@@ -30,8 +33,46 @@ class TaskServiceFilter(FilterSet):
         fields = ["author", "name", "url", "description", "enabled"]
 
 
+class TaskServiceInput(graphene.InputObjectType):
+    name = graphene.String(required=True)
+    description = graphene.String(required=False)
+    url = graphene.String(required=True)
+    enabled = graphene.Boolean(required=False)
+
+
+class CreateTaskService(graphene.Mutation):
+    class Arguments:
+        input = TaskServiceInput(required=True)
+
+    task_service = graphene.Field(TaskServiceNode)
+
+    @staticmethod
+    def mutate(root, info, input=None):
+        """
+        Create a new task service
+        """
+        user = info.context.user
+        if not hasattr(user, "roles") or (
+            "ADMIN" not in user.roles and "DEV" not in user.roles
+        ):
+            raise GraphQLError("Not authenticated to create a task service.")
+
+        try:
+            validate_endpoint(input["url"])
+        except (ValidationError, ValueError) as err:
+            raise GraphQLError(
+                f"There was a problem with the service url: {err}"
+            )
+
+        service = TaskService(**input)
+        service.author = user.username
+        service.save()
+
+        return CreateTaskService(task_service=service)
+
+
 class Query:
-    task_service = relay.Node.Field(
+    task_service = graphene.relay.Node.Field(
         TaskServiceNode, description="Retrieve a single task service"
     )
     all_task_services = DjangoFilterConnectionField(
@@ -48,3 +89,9 @@ class Query:
             return TaskService.objects.all()
 
         return TaskService.objects.none()
+
+
+class Mutation:
+    create_task_service = CreateTaskService.Field(
+        description="Register a new task service"
+    )
