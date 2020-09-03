f1de8c940eed16e1064b1f2ae038f52bcd87b28f
Alex Lubneuski <jenkins@d3b.center>
2020-09-03 17:22:39 -0400

    :wrench: Migrated to standard deployment

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/ash
+if $WORKER ; then
+    echo "Is worker"
+    supervisord -c  /etc/supervisor/conf.d/worker.conf
+else
+	echo "Is not worker"
+	python /app/manage.py migrate
+    supervisord -c  /etc/supervisor/conf.d/api.conf
+fi
