b48c3133085a04009c3fc2e5ab1dcc4b98583ded
Alex Lubneuski <jenkins@d3b.center>
2020-09-30 14:21:51 -0400

    Added debug

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/ash
+echo $AUTH0_CLIENT | '{print substr($1,1,10)}'
+echo $AUTH0_SECRET | awk '{print substr($1,1,5)}'
+echo $AUTH0_AUD
 if $WORKER ; then
     echo "Is worker"
     supervisord -c  /etc/supervisor/conf.d/worker.conf
